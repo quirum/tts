add5ca8ec8f6902c56e2d023b730cf73377f79e4
Alberto Solaro <alberto.solaro@quirum.com>
2019-05-02 19:01:24 +0200

    Pippo

--- functions.inc.php
+++ functions.inc.php
@@ -19,7 +19,7 @@ if ( $tts_agi = file_exists($astlib_path."/agi-bin/propolys-ttsng.agi") ) {
 }
 
 // returns a associative arrays with keys 'destination' and 'description'
-function tts_destinations() {
+function ttsng_destinations() {
 	$results = \FreePBX::Tts()->listTTS();
 
 	// return an associative array with destination and description
@@ -34,12 +34,12 @@ function tts_destinations() {
 	}
 }
 
-function tts_getdestinfo($dest) {
+function ttsng_getdestinfo($dest) {
 	global $amp_conf;
 		if (substr(trim($dest),0,8) == 'ext-tts,') {
 			$tts = explode(',',$dest);
 				$tts = $tts[1];
-				$thistts = tts_get($tts);
+				$thistts = ttsng_get($tts);
 				if (empty($thistts)) {
 					return array();
 				} else {
@@ -52,7 +52,7 @@ function tts_getdestinfo($dest) {
 		}
 }
 
-function tts_get_config($p_var) {
+function ttsng_get_config($p_var) {
 	global $ext;
 
 	switch($p_var) {
@@ -60,13 +60,13 @@ function tts_get_config($p_var) {
 			$contextname = 'ext-ttsng';
 			if ( is_array($tts_list = \FreePBX::Tts()->listTTS()) ) {
 				foreach($tts_list as $item) {
-					$tts = tts_get($item['id']);
+					$tts = ttsng_get($item['id']);
 					$ttsid = $tts['id'];
 					$ttsname= $tts['name'];
 					$ttstext = $tts['text'];
 					$ttsgoto = $tts['goto'];
 					$ttsengine = $tts['engine'];
-					$ttspath = tts_get_ttsengine_path($ttsengine);
+					$ttspath = ttsng_get_ttsengine_path($ttsengine);
 					$ext->add($contextname, $ttsid, '', new ext_noop('TTS: '.$ttsname));
 					$ext->add($contextname, $ttsid, '', new ext_noop('Using: '.$ttsengine));
 					$ext->add($contextname, $ttsid, '', new ext_answer());
@@ -78,7 +78,7 @@ function tts_get_config($p_var) {
 	}
 }
 
-function tts_get_ttsengine_path($engine) {
+function ttsng_get_ttsengine_path($engine) {
 	if (function_exists('ttsengines_get_engine_path')) {
 		return ttsengines_get_engine_path($engine);
 	} else {
@@ -86,12 +86,12 @@ function tts_get_ttsengine_path($engine) {
 	}
 }
 
-function tts_list() {
+function ttsng_list() {
 	dbug('tts_list has been moved in to BMO Tts->listTTS()');
 	return \FreePBX::Tts()->listTTS();
 }
 
-function tts_get($p_id) {
+function ttsng_get($p_id) {
 	global $db;
 
 	$sql = "SELECT id, name, text, goto, engine FROM tts WHERE id=$p_id";
@@ -99,14 +99,14 @@ function tts_get($p_id) {
 	return $res;
 }
 
-function tts_del($p_id) {
+function ttsng_del($p_id) {
 	$dbh = \FreePBX::Database();
 	$sql = 'DELETE FROM tts WHERE id = ?';
 	$stmt = $dbh->prepare($sql);
 	return $stmt->execute(array($p_id));
 }
 
-function tts_add($p_name, $p_text, $p_goto, $p_engine) {
+function ttsng_add($p_name, $p_text, $p_goto, $p_engine) {
 	global $db;
 
 	$tts_list = \FreePBX::Tts()->listTTS();
@@ -123,7 +123,7 @@ function tts_add($p_name, $p_text, $p_goto, $p_engine) {
 	return $db->insert_id();
 }
 
-function tts_update($p_id, $p_name, $p_text, $p_goto, $p_engine) {
+function ttsng_update($p_id, $p_name, $p_text, $p_goto, $p_engine) {
 	$results = sql("UPDATE tts SET name=".sql_formattext($p_name).", text=".sql_formattext($p_text).", goto=".sql_formattext($p_goto).", engine=".sql_formattext($p_engine)." WHERE id=".$p_id);
 }
 
